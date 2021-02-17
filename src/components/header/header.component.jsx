79a73984cfbf568da78af31688cdc15e53e91f35
Matthew Nash <mtntrades10@gmail.com>
2021-02-17 15:11:22 -0800

    Added redux and implemented reducer/actions to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => {
+  console.log("inside header:", currentUser);
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -21,13 +21,17 @@ const Header = ({ currentUser }) => {
         </Link>
         {
           currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className='option' to='/signin'>SIGN IN</Link>
+            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            :
+            <Link className='option' to='/signin'>SIGN IN</Link>
         }
       </div>
     </div>
-  )
+  );
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
