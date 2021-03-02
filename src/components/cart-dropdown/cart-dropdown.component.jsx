f0ca38c69c188f4b3765e70d4e3d0f68ba7ee711
Matthew Nash <mtntrades10@gmail.com>
2021-03-02 12:26:16 -0800

    Added CheckoutItem component and updated Checkout Page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ history, ...otherProps }) => {
   const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
   return (
     <div className='cart-dropdown'>
       <div className='cart-items'>
         {
+          cartItems.length ?
           cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+          :
+          <span className='empty-message'>Your cart is empty</span>
         }
       </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
   );
 }
 
-
-export default CartDropdown;
+export default withRouter(CartDropdown);
