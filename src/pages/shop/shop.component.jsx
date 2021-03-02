5cfecd8ffb42864eebedbb51b282e56c0741785b
Matthew Nash <mtntrades10@gmail.com>
2021-03-02 15:19:04 -0800

    Added directory and shop redux stores

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import SHOP_DATA from './shop.data';
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    );
-  }
+const ShopPage = () => {
+  const collections = useSelector(selectShopCollections);
+  return (
+    <div className='shop-page'>
+      {
+        collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+      }
+    </div>
+  );
 }
 
 export default ShopPage;
